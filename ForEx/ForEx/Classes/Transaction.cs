123684c26c7cebc64c64b75d1c93a085ed7db20b
dllh19 <dllh_19@hotmail.com>
2014-10-26 14:29:43 +0400

    class object for transaction and bug fix for transaction

---
+++ ForEx/ForEx/Classes/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEx.Classes
+{
+    public class Transaction
+    {
+        public int id;
+        public string type;
+        public string currency;
+        public decimal amount;
+        public decimal rates;
+        public decimal total;
+        public DateTime dateCreated;
+        public string username;
+        public int receiptId;
+    }
+}
